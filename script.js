f059094b66eaac956674cdab79477328410d9d5f
Karan S Khedkar <103458258+karanskhedkar@users.noreply.github.com>
2025-05-18 18:32:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here!
 
+let num = 10;
+const scrolling = (e) => {
+  const list = document.getElementById("infi-list");
+  const diff = Math.floor((list.scrollTop + list.clientHeight - list.scrollHeight)**2);
+  console.log(diff)
+  if (diff<=4) {
+    const newEle1 = document.createElement("li");
+    const newEle2 = document.createElement("li");
+    newEle1.textContent = ++num;
+    newEle2.textContent = ++num;
+    list.appendChild(newEle1);
+    list.appendChild(newEle2);
+  }
+};
